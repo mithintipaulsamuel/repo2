05e62cafc4e221fff727907c42e8a524974b4684
mithintipaulsamuel <mithintisamuel99@gmail.com>
2024-01-24 09:51:28 +0530

    Secong commit

---
+++ FibonacciSeries.java
@@ -0,0 +1,18 @@
+
+class FibonacciSeries
+ {
+   public static void main(String args[])
+    {
+     int n1=0,n2=1,n3,i=1;
+     System.out.print(n1+" "+n2);
+     while(i<=20)
+    {
+     n3=n1+n2;     
+    System.out.print(" "+n3);
+      n1=n2;
+      n2=n3;
+      i++;
+    }
+    }
+
+ }
